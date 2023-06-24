b1a6fe8bb0890f8d83c248d4757a16ac6a7ad9d2
Vanh <129242079+vieanh12@users.noreply.github.com>
2023-06-25 01:28:25 +0700

    Add files via upload

---
+++ SQLASS5.sql
@@ -0,0 +1,173 @@
+﻿create database ASS6
+go
+
+
+CREATE TABLE Customers (
+	Customer_ID INT IDENTITY(1,1) NOT NULL UNIQUE,
+	Customer_name NVARCHAR(50) NOT NULL,
+	Address NVARCHAR(255) NOT NULL,
+	Birthday DATE NOT NULL,
+	PRIMARY KEY (Customer_ID)
+);
+
+
+CREATE TABLE PhoneNumbers (
+	Phone_ID INT IDENTITY(1,1) NOT NULL UNIQUE,
+	Customer_ID INT NOT NULL,
+	Numbers INT NOT NULL,
+	PRIMARY KEY (Phone_ID),
+	CONSTRAINT Customer_ID_FK FOREIGN KEY(Customer_ID) REFERENCES Customers(Customer_ID)
+);
+
+INSERT INTO Customers
+	VALUES ('Nguyễn Văn An', '111 Nguyễn Trãi, Thanh Xuân, Hà Nội', '1987-11-18');
+SELECT * FROM Customers;
+
+
+INSERT INTO PhoneNumbers
+	VALUES  (1, 987654321),
+			(1, 09873452),
+			(1, 09832323),
+			(1, 09434343);
+SELECT * FROM PhoneNumbers;
+
+
+SELECT * FROM Customers;
+ 
+
+SELECT * FROM PhoneNumbers;
+
+
+SELECT * FROM Customers
+ORDER BY Customer_name ASC;
+
+
+SELECT pn.* 
+FROM PhoneNumbers pn
+INNER JOIN Customers c ON c.Customer_ID = pn.Customer_ID
+WHERE c.Customer_name = 'Nguyễn Văn An';
+
+
+SELECT * 
+FROM Customers
+WHERE Birthday = '12-12-2009';
+
+
+SELECT c.Customer_name, COUNT(pn.Phone_ID) AS TotalPhoneNumbers
+FROM PhoneNumbers pn
+INNER JOIN Customers c ON c.Customer_ID = pn.Customer_ID
+GROUP BY c.Customer_name;
+
+
+SELECT COUNT(*) AS TotalBirthday12
+FROM Customers
+WHERE MONTH(Birthday) = 12;
+
+
+SELECT * 
+FROM PhoneNumbers pn
+FULL JOIN Customers c ON c.Customer_ID = pn.Customer_ID;
+
+
+SELECT c.*
+FROM PhoneNumbers pn
+INNER JOIN Customers c ON c.Customer_ID = pn.Customer_ID
+WHERE pn.Numbers = 123456789;
+
+
+UPDATE Customers
+SET Birthday = DATEADD(day, -1, GETDATE())
+WHERE Birthday > GETDATE();
+
+
+SELECT COLUMN_NAME, CONSTRAINT_NAME
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'Customers' AND CONSTRAINT_NAME LIKE 'PK_%';
+
+
+SELECT COLUMN_NAME, CONSTRAINT_NAME
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'PhoneNumbers' AND CONSTRAINT_NAME LIKE 'PK_%';
+
+
+SELECT CONSTRAINT_NAME, COLUMN_NAME
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'PhoneNumbers' AND CONSTRAINT_NAME LIKE '%FK';
+
+
+ALTER TABLE Customers
+ADD StartDate DATE;
+SELECT * FROM Customers;
+
+
+CREATE INDEX IX_HoTen
+ON Customers (Customer_name);
+
+
+CREATE INDEX IX_SoDienThoai
+ON PhoneNumbers (Numbers);
+
+
+CREATE VIEW View_SoDienThoai AS
+SELECT c.Customer_name, pn.Numbers
+FROM Customers c
+INNER JOIN PhoneNumbers pn ON c.Customer_ID = pn.Customer_ID;
+ 
+
+SELECT * FROM View_SoDienThoai;
+
+
+CREATE VIEW View_SinhNhat AS
+SELECT c.Customer_name, c.Birthday ,pn.Numbers
+FROM Customers c
+INNER JOIN PhoneNumbers pn ON c.Customer_ID = pn.Customer_ID
+WHERE MONTH(Birthday) = GETDATE();
+
+SELECT * FROM View_SinhNhat;
+
+
+CREATE Procedure SP_Them_DanhBa
+	@Customer_name NVARCHAR(50),
+	@Address NVARCHAR(255),
+	@Birthday DATE
+AS
+BEGIN
+	INSERT INTO Customers(Customer_name, Address, Birthday)
+		VALUES (@Customer_name, @Address, @Birthday)
+	BEGIN
+	PRINT 'ADD NEW SUCCESS';
+	END
+END;
+
+--RUN
+EXEC SP_Them_DanhBa 'Phạm Văn Nam', '8A Tôn Thất Thuyết, Cầu Giấy, Hà Nội', '2002-5-9';
+EXEC SP_Them_DanhBa 'Phạm Bao Cuong', 'Đống Đa, Hà Nội', '2002-5-9';
+EXEC SP_Them_DanhBa 'Phạm Văn Nam', '8A Tôn Thất Thuyết, Cầu Giấy, Hà Nội', '2002-5-9';
+SELECT * FROM Customers;
+
+
+CREATE Procedure SP_Tim_DanhBa
+	@Customer_name NVARCHAR(50)
+AS
+BEGIN
+	IF EXISTS (
+		SELECT *
+		FROM Customers
+		WHERE Customer_name LIKE '%' + @Customer_name + '%'
+	)
+	BEGIN
+		SELECT *
+		FROM Customers
+		WHERE Customer_name LIKE '%' + @Customer_name + '%';
+	END
+	ELSE
+	BEGIN
+		PRINT 'Không tìm được tên nào phù hợp'
+	END
+END;
+--TEST
+SELECT * FROM Customers;
+EXEC SP_Tim_DanhBa 'AN';
+EXEC SP_Tim_DanhBa 'NAM';
+EXEC SP_Tim_DanhBa 'TRẦN';
+EXEC SP_Tim_DanhBa 'PHẠM';
---
+++ SQLASS6.sql
@@ -0,0 +1,258 @@
+﻿create database ASS5
+CREATE TABLE PublishingCompany (
+	Company_ID INT IDENTITY (1, 1) NOT NULL UNIQUE,
+	Company_name NVARCHAR(50) NOT NULL,
+	Address NVARCHAR(255) NOT NULL,
+	Quantity INT NOT NULL,
+	PRIMARY KEY (Company_ID)
+);
+
+
+CREATE TABLE Catalogies (
+	Catalogi_ID INT IDENTITY (1, 1) NOT NULL UNIQUE,
+	Catalogi_name NVARCHAR(50) NOT NULL,
+	PRIMARY KEY (Catalogi_ID)
+);
+
+
+CREATE TABLE Author (
+	Author_ID INT IDENTITY (1, 1) NOT NULL UNIQUE,
+	Author_name NVARCHAR(50) NOT NULL,
+	PRIMARY KEY (Author_ID)
+);
+
+CREATE TABLE Book (
+	Book_ID NVARCHAR(20) NOT NULL UNIQUE,
+	Author_ID INT NOT NULL,
+	Company_ID INT NOT NULL,
+	Catalogi_ID INT NOT NULL,
+	Book_name NVARCHAR(50) NOT NULL,
+	Summary_content NVARCHAR(MAX) NOT NULL,
+	Publishing_year INT NOT NULL,
+	Publication_time INT NOT NULL,
+	UnitPrice MONEY NOT NULL,
+	PRIMARY KEY (Book_ID),
+	CONSTRAINT Author_ID_FK FOREIGN KEY(Author_ID) REFERENCES Author(Author_ID),
+	CONSTRAINT Company_ID_FK FOREIGN KEY(Company_ID) REFERENCES PublishingCompany(Company_ID),
+	CONSTRAINT Catalogi_ID_FK FOREIGN KEY(Catalogi_ID) REFERENCES Catalogies(Catalogi_ID)
+);
+
+CREATE TABLE AuthorBook (
+	Book_ID NVARCHAR(20) NOT NULL,
+	Author_ID INT NOT NULL,
+	PRIMARY KEY (Book_ID, Author_ID),
+	FOREIGN KEY(Book_ID) REFERENCES Book(Book_ID),
+	FOREIGN KEY(Author_ID) REFERENCES Author(Author_ID)
+);
+
+INSERT INTO PublishingCompany
+	VALUES ('Tri Thức', '53 Nguyễn Du, Hai Bà Trưng, Hà Nội', 100);
+SELECT * FROM PublishingCompany;
+
+
+INSERT INTO Catalogies 
+	VALUES ('Khoa học xã hội');
+SELECT * FROM Catalogies;
+
+INSERT INTO Author 
+	VALUES ('Eran Katz');
+SELECT * FROM Author;
+
+
+INSERT INTO Book (Book_ID, Author_ID, Company_ID, Catalogi_ID, Book_name, Summary_content, Publishing_year, Publication_time, UnitPrice) 
+	VALUES ('B001', 1, 1, 1,'Trí tuệ Do Thái', 'Bạn có muốn biết: Người Do Thái sáng tạo ra cái gì và nguồn gốc
+trí tuệ của họ xuất phát từ đâu không? Cuốn sách này sẽ dần hé lộ
+những bí ẩn về sự thông thái của người Do Thái, của một dân tộc
+thông tuệ với những phương pháp và kỹ thuật phát triển tầng lớp trí
+thức đã được giữ kín hàng nghìn năm như một bí ẩn mật mang tính
+văn hóa.', 2010, 1, 79000);
+SELECT * FROM Book;
+
+
+SELECT * 
+FROM Book
+WHERE Publishing_year BETWEEN 2008 AND 2023; 
+
+
+SELECT MAX(UnitPrice) AS MaxUnitPrice
+FROM Book
+
+
+SELECT * FROM Book
+WHERE Book_name Like '%tin học%';
+
+SELECT * FROM Book
+WHERE Book_name Like '%do%';
+
+
+SELECT * FROM Book
+WHERE Book_name LIKE 'T%';
+
+
+SELECT b.*
+FROM Book b
+INNER JOIN PublishingCompany pc ON pc.Company_ID = b.Company_ID
+WHERE pc.Company_name = 'Tri thức';
+
+SELECT *
+FROM Book
+WHERE Company_ID IN (SELECT Company_ID FROM PublishingCompany WHERE Company_name LIKE 'Tri thức')
+
+
+SELECT *
+FROM PublishingCompany
+WHERE Company_ID IN (SELECT Company_ID FROM Book WHERE Book_name LIKE 'Trí tuệ Do Thái');
+
+
+SELECT b.Book_ID, b.Book_name, b.Publishing_year, pc.Company_name, c.Catalogi_name
+FROM Book b
+INNER JOIN Catalogies c ON c.Catalogi_ID = b.Catalogi_ID
+INNER JOIN PublishingCompany pc ON pc.Company_ID = b.Company_ID;
+
+
+SELECT TOP 1 Book_name, UnitPrice
+FROM Book
+ORDER BY UnitPrice DESC;
+
+SELECT Book_name
+FROM Book
+WHERE Company_ID IN (SELECT TOP 1 Company_ID FROM PublishingCompany ORDER BY Quantity DESC);
+
+SELECT TOP 1 b.Book_name, Book_name
+FROM Book b
+INNER JOIN PublishingCompany pc ON pc.Company_ID = b.Company_ID
+ORDER BY Quantity DESC;
+ 
+
+SELECT Book_name
+FROM Book
+WHERE Author_ID IN (SELECT Author_ID FROM Author WHERE Author_name LIKE 'Eran Katz');
+
+
+UPDATE Book 
+SET UnitPrice = UnitPrice * 0.9
+WHERE Publishing_year <= 2008;
+
+
+SELECT pc.Company_name, COUNT(*) AS TotalBook
+FROM Book b
+INNER JOIN PublishingCompany pc ON pc.Company_ID = b.Company_ID
+GROUP BY pc.Company_name;
+
+
+SELECT c.Catalogi_name, COUNT(*) AS TotalBook
+FROM Book b
+INNER JOIN Catalogies c ON c.Catalogi_ID = b.Catalogi_ID
+GROUP BY c.Catalogi_name;
+
+
+CREATE INDEX idc_BookName
+ON Book (Book_name);
+
+
+CREATE VIEW View_Infor_Book AS
+SELECT b.Book_ID, b.Book_name, a.Author_name, pc.Company_name, b.UnitPrice
+FROM Book b
+INNER JOIN PublishingCompany pc ON pc.Company_ID = b.Company_ID
+INNER JOIN Author a ON a.Author_ID = b.Author_ID;
+
+SELECT * FROM View_Infor_Book;
+
+
+CREATE PROCEDURE SP_Them_Sach
+	@Book_ID NVARCHAR(20),
+	@Author_ID INT,
+	@Company_ID INT ,
+	@Catalogi_ID INT ,
+	@Book_name NVARCHAR(50),
+	@Summary_content NVARCHAR(MAX),
+	@Publishing_year INT ,
+	@Publication_time INT ,
+	@UnitPrice MONEY
+AS
+BEGIN
+	IF EXISTS( SELECT * FROM Book WHERE Book_ID != @Book_ID AND @Author_ID IN (SELECT Author_ID FROM Author) 
+				AND @Company_ID IN (SELECT Company_ID FROM PublishingCompany) AND @Catalogi_ID  IN (SELECT Catalogi_ID FROM Catalogies) )
+		INSERT INTO Book(Book_ID, Author_ID, Company_ID, Catalogi_ID, Book_name, Summary_content, Publishing_year, Publication_time, UnitPrice) 
+			VALUES  (@Book_ID, @Author_ID, @Company_ID, @Catalogi_ID, @Book_name, @Summary_content, @Publishing_year, @Publication_time, @UnitPrice);
+	ELSE
+	BEGIN
+		PRINT 'Du lieu dua vao khong hop le'
+	END
+END;
+
+EXEC SP_Them_Sach 'B002', 1, 1, 1, 'Đắc Nhân Tâm', 'giao tiếp', 2006, 10, 120000;
+EXEC SP_Them_Sach 'B003', 2, 2, 3, 'Tấm cám', 'truyện cổ tích', 2001, 5, 100000;
+
+
+CREATE PROCEDURE SP_Tim_Sach
+	@Keywork NVARCHAR(50)
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM Book WHERE Book_name LIKE '%' + @Keywork + '%')
+		SELECT * FROM Book WHERE Book_name LIKE '%' + @Keywork + '%'
+	ELSE
+	BEGIN
+		PRINT 'Du lieu dua vao khong tim thay'
+	END
+END;
+
+DROP PROCEDURE SP_Tim_Sach;
+
+EXEC SP_Tim_Sach 'trí';
+EXEC SP_Tim_Sach 'do';
+EXEC SP_Tim_Sach 'z';
+
+
+CREATE PROCEDURE SP_Sach_ChuyenMuc
+	@Catalogi_ID INT
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM Book WHERE @Catalogi_ID IN ( SELECT Catalogi_ID FROM Catalogies))
+		SELECT * FROM Book WHERE @Catalogi_ID IN ( SELECT Catalogi_ID FROM Catalogies)
+	BEGIN
+		PRINT 'Du lieu dua vao khong tim thay'
+	END
+END;
+
+EXEC SP_Sach_ChuyenMuc 1;
+EXEC SP_Sach_ChuyenMuc 2;
+
+
+CREATE TRIGGER TG_Xoa_Sach
+ON Book AFTER DELETE
+AS
+BEGIN
+		IF EXISTS (SELECT * FROM PublishingCompany pc
+					INNER JOIN deleted d ON d.Company_ID = pc.Company_ID
+					WHERE pc.Quantity > 0
+					)
+	BEGIN
+		RAISERROR('Not allowed to delete books', 16, 1)
+		ROLLBACK TRANSACTION
+	END
+END;
+
+SELECT * FROM PublishingCompany;
+DELETE Book WHERE Company_ID IN ( SELECT Company_ID FROM PublishingCompany)
+
+
+CREATE TRIGGER TG_Xoa_Danh_Muc
+ON Catalogies AFTER DELETE
+AS
+BEGIN
+	IF EXISTS ( SELECT * FROM Book b
+				INNER JOIN deleted d ON b.Catalogi_ID = d.Catalogi_ID 
+				)
+	BEGIN
+		RAISERROR('It is not allowed to delete the list of books with books in this category', 16, 1)
+		ROLLBACK TRANSACTION
+	END
+	ELSE
+	BEGIN
+		DELETE c
+		FROM Catalogies c
+		INNER JOIN deleted d ON C.Catalogi_ID = d.Catalogi_ID;
+	END
+END;
+
